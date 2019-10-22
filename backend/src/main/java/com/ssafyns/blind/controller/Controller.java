be1ad984ff71ec78062b50e22106b1a432d6a7ad
ezslvr <jieun08@naver.com>
2019-10-22 14:39:59 +0900

    create basic Vo class data

---
+++ backend/src/main/java/com/ssafyns/blind/controller/Controller.java
@@ -0,0 +1,11 @@
+package com.ssafyns.blind.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
+public class Controller {
+
+}
---
+++ backend/src/main/java/com/ssafyns/blind/vo/FreeboardCommentVo.java
@@ -0,0 +1,29 @@
+package com.ssafyns.blind.vo;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@Builder
+@NoArgsConstructor
+@Alias("freeboard_comment")
+public class FreeboardCommentVo {
+    private Long fbcommentNo;
+    private String fbcommentContent;
+    private String fbcommentDate;
+    private int fbcommentLikes;
+    private Long fbcommentFreeboardNo;
+    private Long fbcommentFreeboardAccountId;
+    private Long fbcommentAccountId;
+
+    public FreeboardCommentVo(String fbcommentContent, String fbcommentDate, int fbcommentLikes, Long fbcommentFreeboardNo, Long fbcommentFreeboardAccountId, Long fbcommentAccountId) {
+        this.fbcommentContent = fbcommentContent;
+        this.fbcommentDate = fbcommentDate;
+        this.fbcommentLikes = fbcommentLikes;
+        this.fbcommentFreeboardNo = fbcommentFreeboardNo;
+        this.fbcommentFreeboardAccountId = fbcommentFreeboardAccountId;
+        this.fbcommentAccountId = fbcommentAccountId;
+    }
+}
