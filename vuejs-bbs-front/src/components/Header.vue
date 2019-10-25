62188f7bd7676f8a260178bf7a8169091c45911a
kerochuu <kjh14541@naver.com>
2019-10-25 17:46:32 +0900

    Req. 2-1 gitlab과 연동 된 사용자 인증기능

--- vuejs-bbs-front/src/components/Header.vue
+++ vuejs-bbs-front/src/components/Header.vue
@@ -4,12 +4,18 @@
       <div class="nav-wrapper container teal">
         <router-link to="/" class="brand-logo">IDPRAVUS</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><router-link to="/board?page=0">게시판</router-link></li>
-          <li><router-link to="/user">회원 목록</router-link></li>
-          <li v-if="!loggedIn"><router-link to="/signup">회원 가입</router-link></li>
-          <li v-if="!loggedIn"><a @click="login">로그인</a></li>
-          <li v-if="loggedIn"><router-link to="/user/modify">정보수정</router-link></li>
-          <li v-if="loggedIn"><a @click="logout">로그아웃</a></li>
+          <li>
+            <router-link to="/board?page=0">게시판</router-link>
+          </li>
+          <li v-if="!loggedIn">
+            <router-link to="/signup">회원 가입</router-link>
+          </li>
+          <li v-if="!loggedIn">
+            <a @click="login">로그인</a>
+          </li>
+          <li v-if="loggedIn">
+            <a @click="logout">로그아웃</a>
+          </li>
         </ul>
       </div>
     </nav>
@@ -17,8 +23,8 @@
 </template>
 
 <script>
-import qstr from 'query-string';
-import axios from '../libs/axios.custom';
+import qstr from "query-string";
+import axios from "../libs/axios.custom";
 
 export default {
   async created() {
@@ -28,33 +34,35 @@ export default {
   data: () => ({
     loggedIn: false,
     params: {
-      response_type: 'token',
-      client_id: 'vueclient',
-      redirect_uri: `${process.env.VUE_APP_ORIGIN}/login?success`,
+      response_type: "token",
+      client_id: "vueclient",
+      redirect_uri: `${process.env.VUE_APP_ORIGIN}/login?success`
     },
-    originHost: process.env.VUE_APP_ORIGIN,
+    originHost: process.env.VUE_APP_ORIGIN
   }),
 
   methods: {
     async login() {
-      const url = `${process.env.VUE_APP_API}/oauth/authorize?${qstr.stringify(this.params)}`;
-      const options = 'width=600, height=600';
-      const popup = window.open(url, 'auth', options);
+      const url = `${process.env.VUE_APP_API}/oauth/authorize?${qstr.stringify(
+        this.params
+      )}`;
+      const options = "width=600, height=600";
+      const popup = window.open(url, "auth", options);
 
       const param = await this.popupWatcher(popup, this.originHost);
-      await this.$store.commit('setUser', param);
+      await this.$store.commit("setUser", param);
       await this.getUserDetails();
     },
 
     popupWatcher(popup, exitUrl) {
-      const parseUrl = document.createElement('a');
+      const parseUrl = document.createElement("a");
       parseUrl.href = exitUrl;
 
       return new Promise((resolve, reject) => {
         const polling = setInterval(() => {
           if (!popup || popup.closed || popup === undefined) {
             clearInterval(polling);
-            reject(new Error('로그인 창 종료됨'));
+            reject(new Error("로그인 창 종료됨"));
           }
 
           try {
@@ -82,7 +90,7 @@ export default {
 
         const res = await axios.get(`/api/v1/user/${username}`);
         if (res.status === 200) {
-          this.$store.commit('setUserDetail', res.data);
+          this.$store.commit("setUserDetail", res.data);
           this.loggedIn = true;
         }
       } catch (err) {
@@ -94,11 +102,10 @@ export default {
 
     logout() {
       this.loggedIn = false;
-      this.$store.commit('setUser', null);
-      this.$store.commit('setUserDetail', null);
-      this.$router.push('/');
-    },
-  },
-
+      this.$store.commit("setUser", null);
+      this.$store.commit("setUserDetail", null);
+      this.$router.push("/");
+    }
+  }
 };
 </script>
--- vuejs-bbs-front/src/components/user/Signup.vue
+++ vuejs-bbs-front/src/components/user/Signup.vue
@@ -6,35 +6,47 @@
     <div class="row">
       <div class="row">
         <div class="input-field col s12">
-          <input id="username" v-model="user.username"
-            type="text" class="validate" placeholder="사용자 아이디">
+          <input
+            id="username"
+            v-model="user.username"
+            type="text"
+            class="validate"
+            placeholder="사용자 아이디"
+          />
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="password" v-model="user.password" type="password"
-            class="validate" placeholder="비밀번호">
+          <input
+            id="password"
+            v-model="user.password"
+            type="password"
+            class="validate"
+            placeholder="비밀번호"
+          />
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="passwordre" v-model="user.confirmpassword" type="password"
-            class="validate" placeholder="비밀번호 확인">
+          <input
+            id="passwordre"
+            v-model="user.confirmpassword"
+            type="password"
+            class="validate"
+            placeholder="비밀번호 확인"
+          />
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s12">
-          <input id="name" v-model="user.name" type="text" class="validate"
-            placeholder="이름">
-        </div>
+        <div class="input-field col s12">해시 코드 : {{hashCode}}</div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" v-model="user.email" type="email" class="validate"
-            placeholder="이메일">
-        </div>
-      </div>
-      <a class="btn waves-effect waves-light" @click="signup">회원가입
+      <a class="btn waves-effect waves-light" @click="isExist">
+        페이지 확인
+        <i class="material-icons right">send</i>
+      </a>
+
+      <a class="btn waves-effect waves-light" @click="signup">
+        회원가입
         <i class="material-icons right">send</i>
       </a>
     </div>
@@ -42,38 +54,108 @@
 </template>
 
 <script>
-import axios from '../../libs/axios.custom';
-import notification from '../../libs/notification';
+import axios from "../../libs/axios.custom";
+import notification from "../../libs/notification";
 
 export default {
   data: () => ({
     user: {
-      username: '',
-      password: '',
-      confirmpassword: '',
-      name: '',
-      email: '',
+      username: "",
+      password: "",
+      confirmpassword: ""
     },
+    hashCode: 0
   }),
+
+  created() {
+    this.createHash();
+  },
 
   methods: {
     async signup() {
       try {
-        const res = await axios.post('/api/v1/user/signup', this.user);
-        notification.success(res, '가입성공', () => {
-          this.$router.push('/');
+        const res = await axios.post("/api/v1/user/signup", this.user);
+        notification.success(res, "가입성공", () => {
+          this.$router.push("/");
         });
       } catch (err) {
-        err.response.data.errors.forEach((error) => {
+        err.response.data.errors.forEach(error => {
           this.$notify({
-            group: 'noti',
-            type: 'error',
+            group: "noti",
+            type: "error",
             duration: 6000,
-            text: error.defaultMessage,
+            text: error.defaultMessage
           });
         });
       }
     },
-  },
+    async isExist() {
+      var temp = this.user.username;
+      var flag = false;
+      var xhr = new XMLHttpRequest();
+      var paramVal = this.hashCode;
+      console.log("hashCode = " + paramVal);
+      var target = "https://lab.ssafy.com/api/v4/projects/";
+      xhr.open("GET", target + "?search=" + paramVal);
+      xhr.send();
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status == 200) {
+            let loadedJSON = JSON.parse(xhr.responseText);
+
+            for (var i = 0; i < Object.keys(loadedJSON).length; i++) {
+              var info = loadedJSON[i].http_url_to_repo
+                .substr(22, loadedJSON[i].http_url_to_repo.length)
+                .split("/");
+
+              console.log("찾아 온 값 = " + info[0] + "  입력 된 값 = " + temp);
+              if (info[0] == temp) {
+                console.log("flag true");
+                flag = true;
+              }
+            }
+            if (flag) {
+              alert("매칭성공");
+            }
+          } else {
+            alert("fail to load");
+          }
+        }
+      };
+
+      // var obj = JSON.parse(
+      //   "https://lab.ssafy.com/api/v4/projects/?search=210105626"
+      // );
+      // console.log(obj.id);
+    },
+    createHash() {
+      var i, chr;
+
+      var currentDateWithFormat = new Date()
+        .toJSON()
+        .replace(/-/g, "")
+        .replace(/:/g, "");
+      console.log(currentDateWithFormat);
+
+      for (i = 0; i < currentDateWithFormat.length; i++) {
+        chr = currentDateWithFormat.charCodeAt(i);
+        this.hashCode = (this.hashCode << 5) - this.hashCode + chr;
+        this.hashCode = this.hashCode & this.hashCode;
+      }
+      if (this.hashCode < 0) {
+        this.hashCode *= -1;
+      }
+      console.log(this.hashCode);
+    },
+    hex2a(hex) {
+      var str = "";
+      for (var i = 0; i < hex.length; i += 2) {
+        var v = parseInt(hex.substr(i, 2), 16);
+        if (v) str += String.fromCharCode(v);
+      }
+      return str;
+    }
+  }
 };
 </script>
